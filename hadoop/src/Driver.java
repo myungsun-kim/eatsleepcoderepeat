fc17f8443836f6fcba4be189b6b43e20f1446b5e
daebalprime <daebalprime@gmail.com>
2021-09-02 18:40:09 -0700

    project3
    Former-commit-id: 262ae2b46fee195f711d9da749f6b33bfbeb5f31

--- hadoop/src/Driver.java
+++ hadoop/src/Driver.java
@@ -10,6 +10,7 @@ public class Driver {
 			pgd.addClass("wordcount",Wordcount.class,"");
 			pgd.addClass("wordcount1char",Wordcount1char.class,"Counts first character of words.");
 			pgd.addClass("wordcountsort",Wordcountsort.class,"sort by alphabetical order.");
+			pgd.addClass("wordcountinvertedindex", WordcountInvertedIndex.class,"get each words location and offsets.");
 
 			pgd.addClass("matrixadd", MatrixAdd.class, "add matrix");
 			pgd.addClass("kmeans",Kmeans.class,"");
