65d80de3ab53a9df445689a4f4870522658d3080
daebalprime <daebalprime@gmail.com>
2021-09-02 18:12:02 -0700

    project4
    Former-commit-id: daff6c783ec56e70c30c3398b0c2c45b52314800

--- hadoop/src/Driver.java
+++ hadoop/src/Driver.java
@@ -9,8 +9,9 @@ public class Driver {
 		try {
 			pgd.addClass("wordcount",Wordcount.class,"");
 			pgd.addClass("wordcount1char",Wordcount1char.class,"Counts first character of words.");
-
 			pgd.addClass("wordcountsort",Wordcountsort.class,"sort by alphabetical order.");
+
+			pgd.addClass("matrixadd", MatrixAdd.class, "add matrix");
 			pgd.addClass("kmeans",Kmeans.class,"");
       			pgd.driver(args);
 			exitCode = 0;
