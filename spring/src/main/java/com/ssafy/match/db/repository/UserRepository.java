b9fb69ea7e9eebce0b3e12fae6a8141b5bd347e7
ilminmoon <ian.ilminmoon@gmail.com>
2021-09-06 10:22:42 +0900

    userrepository update
    Former-commit-id: 09c7b32f5872cd93bd4d45a62e84b0260d1858b2

--- spring/src/main/java/com/ssafy/match/db/repository/UserRepository.java
+++ spring/src/main/java/com/ssafy/match/db/repository/UserRepository.java
@@ -1,6 +1,6 @@
 package com.ssafy.match.db.repository;
 
-import com.ssafy.db.entity.User;
+import com.ssafy.match.db.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Integer> {
