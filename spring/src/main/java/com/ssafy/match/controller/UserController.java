6e5c2baf1b2bedac9b4d236b622e03f30d7d16e1
daebalprime <daebalprime@gmail.com>
2021-09-07 14:00:53 +0900

    basic swagger settings and write examples
    Former-commit-id: 47946fe6eff02d3a01b8a08cefce904f7dd78b3f

--- spring/src/main/java/com/ssafy/match/controller/UserController.java
+++ spring/src/main/java/com/ssafy/match/controller/UserController.java
@@ -2,14 +2,27 @@ package com.ssafy.match.controller;
 
 import com.ssafy.match.db.entity.User;
 import com.ssafy.match.db.repository.UserRepository;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
+@RequiredArgsConstructor
+@CrossOrigin("*")
 @RestController
-@RequestMapping("/api")
+@RequestMapping("/user")
 public class UserController {
     @Autowired
     UserRepository userRepository;
@@ -17,7 +30,27 @@ public class UserController {
     @PostMapping("/join")
     public String join(@RequestBody User user) {
         User newUser = userRepository.save(user);
-        return user.getNickname() + "님의 회원가입을 환영합니다.";
+        return user.getNickname() + "님의 회원가입을 환영합니다?";
+    }
+
+    @ApiOperation(value = "테스트용 API")
+    @GetMapping("/example/{example_id}")
+//    @ResponseBody
+//    @ApiImplicitParam(name = "pk_idx",
+//        value="유저의 이메일이 아닌 고유 회원번호, db에 저장되는 int 타입의 pk_idx",
+//        example = "1")
+//    @ApiResponses(value = {
+//        @ApiResponse(code = 200,
+//            message = "성공",
+//            response = String.class,
+//            responseContainer = "List"),
+//        @ApiResponse(code = 404, message = "낫파운드"),
+//    })
+    public ResponseEntity<?> test(@PathVariable String example_id){
+        if("1".equals(example_id)){
+            return new ResponseEntity<String>("example success!", HttpStatus.OK);
+        }
+        return new ResponseEntity<String>("example failed!", HttpStatus.NOT_FOUND);
     }
 
 }
