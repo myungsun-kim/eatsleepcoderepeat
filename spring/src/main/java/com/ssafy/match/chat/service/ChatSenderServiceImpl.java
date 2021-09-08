d2224a066873b2d3eb9721b0f7abb59205141524
daebalprime <daebalprime@gmail.com>
2021-09-08 17:02:58 +0900

    get basic skeleton for kafka but not working
    Former-commit-id: 543931983d766794192b93a7c3d1cd7a13efa312

---
+++ spring/src/main/java/com/ssafy/match/chat/service/ChatSenderServiceImpl.java
@@ -0,0 +1,22 @@
+package com.ssafy.match.chat.service;
+
+import com.ssafy.match.chat.dto.ChattingMessage;
+import org.apache.kafka.clients.producer.internals.Sender;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ChatSenderServiceImpl {
+    private static final Logger LOGGER = LoggerFactory.getLogger(ChatSenderServiceImpl.class);
+
+    @Autowired
+    private KafkaTemplate<String, ChattingMessage> kafkaTemplate;
+
+    public void send(String topic, ChattingMessage data) {
+        LOGGER.info("sending data='{}' to topic='{}'", data, topic);
+        kafkaTemplate.send(topic, data);// send to react clients via websocket(STOMP)
+    }
+}
