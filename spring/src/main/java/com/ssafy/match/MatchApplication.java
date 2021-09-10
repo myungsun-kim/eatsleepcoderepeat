b8cb5c29906c2557efa493e2d2d9a43ee4938b02
박범진 <qjawlsqjacks@naver.com>
2021-09-10 15:56:59 +0900

    feat: 파일 업로드, 다운로드
    Former-commit-id: 335fd047b6174cb38dea267e3edd33de9e8520fb

--- spring/src/main/java/com/ssafy/match/MatchApplication.java
+++ spring/src/main/java/com/ssafy/match/MatchApplication.java
@@ -1,14 +1,9 @@
 package com.ssafy.match;
 
-import com.ssafy.match.config.FileStorageConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
-@EnableConfigurationProperties(
-    {FileStorageConfig.class}
-)
 public class MatchApplication {
 
     public static void main(String[] args) {
--- spring/src/main/java/com/ssafy/match/service/FileStorageService.java
+++ spring/src/main/java/com/ssafy/match/service/FileStorageService.java
@@ -1,69 +1,87 @@
 package com.ssafy.match.service;
 
-import com.ssafy.match.config.FileStorageConfig;
-import com.ssafy.match.exception.FileStorageException;
-import com.ssafy.match.exception.MyFileNotFoundException;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.io.Resource;
-import org.springframework.core.io.UrlResource;
-import org.springframework.stereotype.Service;
-import org.springframework.util.StringUtils;
-import org.springframework.web.multipart.MultipartFile;
 import java.io.IOException;
+import java.io.InputStream;
 import java.net.MalformedURLException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.StandardCopyOption;
+import java.util.stream.Stream;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.UrlResource;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
 @Service
-public class FileStorageService {
+public class FileStorageService implements StorageService {
 
-    private final Path fileStorageLocation;
+    @Value("${spring.servlet.multipart.location}")
+    private String uploadPath;
 
-    @Autowired
-    public FileStorageService(FileStorageConfig fileStorageProperties) {
-        this.fileStorageLocation = Paths.get(fileStorageProperties.getUploadDir())
-            .toAbsolutePath().normalize();
-
+    @Override
+    public void init() {
         try {
-            Files.createDirectories(this.fileStorageLocation);
-        } catch (Exception ex) {
-            throw new FileStorageException("Could not create the directory where the uploaded files will be stored.", ex);
+            Files.createDirectories(Paths.get(uploadPath));
+        } catch (IOException e) {
+            throw new RuntimeException("Could not create upload folder!");
         }
     }
 
-    public String storeFile(MultipartFile file) {
-        // Normalize file name
-        String fileName = StringUtils.cleanPath(file.getOriginalFilename());
-
+    @Override
+    public void store(MultipartFile file) {
         try {
-            // Check if the file's name contains invalid characters
-            if(fileName.contains("..")) {
-                throw new FileStorageException("Sorry! Filename contains invalid path sequence " + fileName);
+            if (file.isEmpty()) {
+                throw new Exception("ERROR : File is empty.");
+            }
+            Path root = Paths.get(uploadPath);
+            if (!Files.exists(root)) {
+                init();
             }
 
-            // Copy file to the target location (Replacing existing file with the same name)
-            Path targetLocation = this.fileStorageLocation.resolve(fileName);
-            Files.copy(file.getInputStream(), targetLocation, StandardCopyOption.REPLACE_EXISTING);
-
-            return fileName;
-        } catch (IOException ex) {
-            throw new FileStorageException("Could not store file " + fileName + ". Please try again!", ex);
+            try (InputStream inputStream = file.getInputStream()) {
+                Files.copy(inputStream, root.resolve(file.getOriginalFilename()),
+                    StandardCopyOption.REPLACE_EXISTING);
+            }
+        } catch (Exception e) {
+            throw new RuntimeException("Could not store the file. Error: " + e.getMessage());
         }
     }
 
-    public Resource loadFileAsResource(String fileName) {
+    @Override
+    public Stream<Path> loadAll() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public Path load(String filename) {
+        return Paths.get(uploadPath).resolve(filename);
+    }
+
+    @Override
+    public Resource loadAsResource(String filename) {
         try {
-            Path filePath = this.fileStorageLocation.resolve(fileName).normalize();
-            Resource resource = new UrlResource(filePath.toUri());
-            if(resource.exists()) {
+            Path file = load(filename);
+            Resource resource = new UrlResource(file.toUri());
+            if (resource.exists() || resource.isReadable()) {
                 return resource;
-            } else {
-                throw new MyFileNotFoundException("File not found " + fileName);
             }
-        } catch (MalformedURLException ex) {
-            throw new MyFileNotFoundException("File not found " + fileName, ex);
+            else {
+                throw new RuntimeException("Could not read file: " + filename);
+            }
+        }
+        catch (MalformedURLException e) {
+            throw new RuntimeException("Could not read file: " + filename, e);
         }
     }
+
+    @Override
+    public void deleteAll() {
+        // TODO Auto-generated method stub
+
+    }
+
 }
