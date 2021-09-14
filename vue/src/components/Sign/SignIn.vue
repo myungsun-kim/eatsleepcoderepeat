3510495a4c6fcfa024f6ac2af7a7754f2d6cc582
juns5779 <juns5779@naver.com>
2021-09-14 16:28:46 +0900

    feat: sign임시
    Former-commit-id: 81fa38731d82f0b47e360a6d6511c212040a19e5

---
+++ vue/src/components/Sign/SignIn.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <div class="left">좌</div>
+    <div class="right">우</div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+div {
+  width: 100%;
+  height: 100%;
+
+  border: 1px solid red;
+}
+div.left {
+  width: 50%;
+  height: 100%;
+  float: left;
+  box-sizing: border-box;
+}
+div.right {
+  width: 50%;
+  height: 100%;
+  float: right;
+  box-sizing: border-box;
+}
+</style>
---
+++ vue/src/components/Sign/SignUp.vue
@@ -0,0 +1,9 @@
+<template>
+  <p>SignUp</p>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
