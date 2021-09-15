12d20963535536fc164072b15ef52e7713f585db
neverte <neverte@naver.com>
2021-09-15 13:42:21 +0900

    feat: font cdn 적용 완료
    Former-commit-id: 0570fa22acb567096c90b847f8aadb30d27879bb

--- vue/src/App.vue
+++ vue/src/App.vue
@@ -5,22 +5,26 @@
   </div> -->
 
   <!-- <TopNavigationBar /> -->
+  <div class="font-noto-bold">font-noto-bold 안녕하세요 700</div>
+  <div class="font-noto-md">font-noto-md 안녕하세요 500</div>
+  <div class="font-s-md">font-s-md 안녕하세요</div>
+
+  <el-row :gutter="20">
+    <el-col :span="12" :offset="0" class="simple-bg">test1</el-col>
+    <el-col :span="12" :offset="0" class="simple-bg">test2</el-col>
+  </el-row>
 
   <!-- Main(프로젝트명) 역할 -->
+
   <router-view />
 </template>
 <script>
-// import TopNavigationBar from './views/TopNavigationBar.vue';
-
-export default {
-  // components: {
-  //   TopNavigationBar,
-  // },
-};
+export default {};
 </script>
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: 'Noto Sans KR', 'S-CoreDream-5Medium', '맑은 고딕', Avenir,
+    Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
@@ -38,5 +42,36 @@ export default {
 
 #nav a.router-link-exact-active {
   color: #42b983;
+}
+
+/* font cdn:s-core dream */
+@font-face {
+  font-family: 'S-CoreDream-5Medium';
+  src: url('https://cdn.jsdelivr.net/gh/projectnoonnu/noonfonts_six@1.2/S-CoreDream-5Medium.woff')
+    format('woff');
+  font-weight: normal;
+  font-style: normal;
+}
+
+.font-noto-bold {
+  font-family: 'noto-sans-kr';
+  font-weight: 700;
+  font-size: 60pt;
+}
+.font-noto-md {
+  font-family: 'noto-sans-kr';
+  font-weight: 500;
+  font-size: 60pt;
+}
+
+.font-s-md {
+  font-family: 'S-CoreDream-5Medium';
+  font-size: 60pt;
+}
+
+.simple-bg {
+  background-color: red;
+  border: 1px solid;
+  border-color: blue;
 }
 </style>
