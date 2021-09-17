09668f8d9c1c7371fcec8fcbab4f63e991d63573
김명선 <kms8072@daum.net>
2021-09-18 05:48:25 +0900

    fetat: CreateBoard Layout
    Former-commit-id: a9ca532ec68fc384a8bc10724f9a605b457000be

--- vue/src/components/MenuBar1/Board/CreateBoard.vue
+++ vue/src/components/MenuBar1/Board/CreateBoard.vue
@@ -1,3 +1,21 @@
 <template>
-  <div>create board</div>
+  <el-row class="height100"
+    ><el-col :span="3"></el-col>
+    <el-col :span="19"
+      ><el-row class="height5"
+        ><el-col :span="2">제목:</el-col><el-col :span="22"></el-col> </el-row
+      ><el-row class="height5"> </el-row>
+      <el-row style="height: 65%"> 텍스트에디터 </el-row
+      ><el-row class="height5"> </el-row>
+      <el-row class="height5"
+        ><el-col :span="9"></el-col
+        ><el-col :span="2"><button>생성</button></el-col
+        ><el-col :span="1"></el-col
+        ><el-col :span="2"><button>취소</button></el-col
+        ><el-col :span="10"></el-col
+      ></el-row>
+      <el-row style="height: 20%"> </el-row>
+    </el-col>
+    <el-col :span="2"></el-col>
+  </el-row>
 </template>
