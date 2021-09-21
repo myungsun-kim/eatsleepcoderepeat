453eb3f4f24ff8cc2dbd0fac21ee7f1a274000b0
ilminmoon <ian.ilminmoon@gmail.com>
2021-09-21 20:11:36 +0900

    member repo update
    Former-commit-id: 8e37835a47727781e2b958d3e1485b82dcad0821

--- spring/src/main/java/com/ssafy/match/db/entity/MemberBeginnerTechstack.java
+++ spring/src/main/java/com/ssafy/match/db/entity/MemberBeginnerTechstack.java
@@ -1,2 +1,25 @@
-package com.ssafy.match.db.entity;public class MemberBeginnerTechstack {
+package com.ssafy.match.db.entity;
+
+import com.ssafy.match.db.entity.embedded.CompositeMemberTechstack;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.EmbeddedId;
+import javax.persistence.Entity;
+
+@Getter
+@Setter
+@Entity(name = "matching.member_beginner_techstack")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class MemberBeginnerTechstack {
+
+    @EmbeddedId
+    private CompositeMemberTechstack compositeMemberTechstack;
+
+    public MemberBeginnerTechstack(
+            CompositeMemberTechstack compositeMemberTechstack) {
+        this.compositeMemberTechstack = compositeMemberTechstack;
+    }
 }
--- spring/src/main/java/com/ssafy/match/db/repository/MemberExperiencedTechstackRepository.java
+++ spring/src/main/java/com/ssafy/match/db/repository/MemberExperiencedTechstackRepository.java
@@ -1,10 +1,10 @@
 package com.ssafy.match.db.repository;
 
-import com.ssafy.match.db.entity.MemberInterestTechstack;
+import com.ssafy.match.db.entity.MemberExperiencedTechstack;
 import com.ssafy.match.db.entity.embedded.CompositeMemberTechstack;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface MemberInterestTechstackRepository extends
-    JpaRepository<MemberInterestTechstack, CompositeMemberTechstack> {
+public interface MemberExperiencedTechstackRepository extends
+    JpaRepository<MemberExperiencedTechstack, CompositeMemberTechstack> {
 
 }
