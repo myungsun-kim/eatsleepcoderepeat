6ae02483cc8ba163a8258407b58e01ea7edd6776
daebalprime <daebalprime@gmail.com>
2021-09-23 14:59:40 +0900

    new app.jar build
    Former-commit-id: ed554c13f6104c07b204b15ecaea70026fd6e1ab

--- spring/src/main/java/com/ssafy/match/config/SwaggerConfig.java
+++ spring/src/main/java/com/ssafy/match/config/SwaggerConfig.java
@@ -58,6 +58,8 @@ public class SwaggerConfig {
             // api 필요한 클래스패스 추가하기
             .paths(
                         PathSelectors.ant("/**/member/**")
+                        .or(PathSelectors.ant("/**/auth/**"))
+
 //						.or(PathSelectors.ant("/**/chat/**"))
 //                PathSelectors.any()
             )
@@ -76,8 +78,7 @@ public class SwaggerConfig {
             // api 필요한 클래스패스 추가하기
             .paths(
 
-                        PathSelectors.ant("/**/member/**")
-                        .or(PathSelectors.ant("/**/auth/**"))
+                        PathSelectors.ant("/**/chat/**")
 //						.or(PathSelectors.ant("/**/**"))
 //                PathSelectors.any()
             )
