d5fdfe35a6f3c2df9d356408b9143446df1d3139
neverte <neverte@naver.com>
2021-09-23 11:03:54 +0900

    fix: sub nav bar
    Former-commit-id: 960d77e1051134685c2296b3a7ad05e688099b8c

--- vue/src/components/SubNavBar.vue
+++ vue/src/components/SubNavBar.vue
@@ -1,7 +1,5 @@
 <template>
-<<<<<<< HEAD
-  <!-- <div>Sub Nav Bar</div> -->
-=======
-  <div>Sub Nav Bar</div>
->>>>>>> 4966f77b8cc58f9a84f2b89acb0afb4271956723
+  <div>
+    <!-- Sub Nav Bar -->
+  </div>
 </template>
