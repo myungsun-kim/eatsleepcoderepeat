6ad4f35b96e087cd465c6357e388cf3b95da3a3c
박범진 <qjawlsqjacks@naver.com>
2021-09-27 16:01:06 +0900

    bug: 주석 해제
    Former-commit-id: e83bb795cc7175f169163112bfde7523ace7eb61

--- spring/src/main/java/com/ssafy/match/group/controller/ProjectFormController.java
+++ spring/src/main/java/com/ssafy/match/group/controller/ProjectFormController.java
@@ -26,14 +26,14 @@ public class ProjectFormController {
 
     private final ProjectService projectService;
 
-//    @GetMapping("/forcreate/{projectId}")
-//    @ApiOperation(value = "신청서 생성을 위한 정보", notes = "<strong>프로젝트를에 가입하기 위한</strong>신청서를 작성하기 위한 정보(전체 기술, 선택할 수 있는 지역 리스트)를 받는다")
-//    @ApiResponses({
-//        @ApiResponse(code = 200, message = "성공"),
-//    })
-//    public ResponseEntity<FormtInfoForRegisterResponseDto> checkForRegister(@PathVariable("projectId") Long projectId) throws Exception {
-//        return ResponseEntity.ok(projectService.checkForRegister(projectId));
-//    }
+    @GetMapping("/forcreate/{projectId}")
+    @ApiOperation(value = "신청서 생성을 위한 정보", notes = "<strong>프로젝트를에 가입하기 위한</strong>신청서를 작성하기 위한 정보(전체 기술, 선택할 수 있는 지역 리스트)를 받는다")
+    @ApiResponses({
+        @ApiResponse(code = 200, message = "성공"),
+    })
+    public ResponseEntity<FormtInfoForRegisterResponseDto> checkForRegister(@PathVariable("projectId") Long projectId) throws Exception {
+        return ResponseEntity.ok(projectService.checkForRegister(projectId));
+    }
 
     @PostMapping("/{projectId}")
     @ApiOperation(value = "프로젝트 가입 신청", notes = "<strong>받은 신청서 정보로</strong>를 사용해서 프로젝트에 신청을 한다")
@@ -62,32 +62,32 @@ public class ProjectFormController {
         return ResponseEntity.ok(projectService.reject(projectId, memberId));
     }
 
-//    @GetMapping("/all/{projectId}/{nickname}")
-//    @ApiOperation(value = "특정 프로젝트 닉네임 포함 모든 신청서 조회", notes = "특정 프로젝트의 닉네임 포함 모든 신청서 리스트를 작성일 기준 내림차순으로 받는다")
-//    @ApiResponses({
-//        @ApiResponse(code = 200, message = "성공"),
-//    })
-//    public ResponseEntity<List<FormtInfoResponseDto>> allFormByProjectNickname(@PathVariable("projectId") Long projectId,
-//        @PathVariable("nickname") String nickname) throws Exception {
-//        return ResponseEntity.ok(projectService.allFormByProjectNickname(projectId, nickname));
-//    }
-//
-//    @GetMapping("/all/{projectId}")
-//    @ApiOperation(value = "특정 프로젝트 모든 신청서 조회", notes = "특정 프로젝트의 모든 신청서 리스트를 작성일 기준 내림차순으로 받는다")
-//    @ApiResponses({
-//        @ApiResponse(code = 200, message = "성공"),
-//    })
-//    public ResponseEntity<List<FormtInfoResponseDto>> allProjectForm(@PathVariable("projectId") Long projectId) throws Exception {
-//        return ResponseEntity.ok(projectService.allProjectForm(projectId));
-//    }
-//
-//    @GetMapping("/one/{projectId}/{memberId}")
-//    @ApiOperation(value = "특정 신청서 조회", notes = "특정 신청서를 상세 조회한다.")
-//    @ApiResponses({
-//        @ApiResponse(code = 200, message = "성공"),
-//    })
-//    public ResponseEntity<FormtInfoResponseDto> oneProjectForm(@PathVariable("projectId") Long projectId, @PathVariable("memberId") Long memberId) throws Exception {
-//        return ResponseEntity.ok(projectService.oneProjectForm(projectId, memberId));
-//    }
+    @GetMapping("/all/{projectId}/{nickname}")
+    @ApiOperation(value = "특정 프로젝트 닉네임 포함 모든 신청서 조회", notes = "특정 프로젝트의 닉네임 포함 모든 신청서 리스트를 작성일 기준 내림차순으로 받는다")
+    @ApiResponses({
+        @ApiResponse(code = 200, message = "성공"),
+    })
+    public ResponseEntity<List<FormtInfoResponseDto>> allFormByProjectNickname(@PathVariable("projectId") Long projectId,
+        @PathVariable("nickname") String nickname) throws Exception {
+        return ResponseEntity.ok(projectService.allFormByProjectNickname(projectId, nickname));
+    }
+
+    @GetMapping("/all/{projectId}")
+    @ApiOperation(value = "특정 프로젝트 모든 신청서 조회", notes = "특정 프로젝트의 모든 신청서 리스트를 작성일 기준 내림차순으로 받는다")
+    @ApiResponses({
+        @ApiResponse(code = 200, message = "성공"),
+    })
+    public ResponseEntity<List<FormtInfoResponseDto>> allProjectForm(@PathVariable("projectId") Long projectId) throws Exception {
+        return ResponseEntity.ok(projectService.allProjectForm(projectId));
+    }
+
+    @GetMapping("/one/{projectId}/{memberId}")
+    @ApiOperation(value = "특정 신청서 조회", notes = "특정 신청서를 상세 조회한다.")
+    @ApiResponses({
+        @ApiResponse(code = 200, message = "성공"),
+    })
+    public ResponseEntity<FormtInfoResponseDto> oneProjectForm(@PathVariable("projectId") Long projectId, @PathVariable("memberId") Long memberId) throws Exception {
+        return ResponseEntity.ok(projectService.oneProjectForm(projectId, memberId));
+    }
 
 }
