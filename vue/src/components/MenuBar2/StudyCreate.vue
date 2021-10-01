e07499a7593e3511fbc58d44ec49d947a51b6453
neverte <neverte@naver.com>
2021-10-01 17:51:19 +0900

    fix: compile error
    Former-commit-id: 9cdcb1f61a1bffa20f643550e796aead1fc3b8fe

--- vue/src/components/MenuBar2/StudyCreate.vue
+++ vue/src/components/MenuBar2/StudyCreate.vue
@@ -190,7 +190,6 @@ export default {
     // console.log(user.value.myClubList.length);
     // console.log(user.value.myClubList[0]);
 
-<<<<<<< HEAD
     // let clubList = [];
     // let clubId = [];
     // if (user.value.myClubList.length > 0) {
@@ -202,19 +201,6 @@ export default {
     //   clubList[0] = '없음';
     //   clubId[0] = null;
     // }
-=======
-    let clubList = [];
-    let clubId = [];
-    if (user.value.myClubList.length > 0) {
-      for (let index = 0; index < user.value.myClubList.length; index++) {
-        clubList[index] = user.value.myClubList[index].name;
-        clubId[index] = user.value.myClubList[index].id;
-      }
-    } else {
-      clubList[0] = '무관';
-      clubId[0] = null;
-    }
->>>>>>> fe6e12da89258d26db6191f73c5fdeb00f17ecb5
     // const clubList = ['사과', '배클럽', '우주', '공장'];
     // const clubId = [31, 32, 33, 34];
 
