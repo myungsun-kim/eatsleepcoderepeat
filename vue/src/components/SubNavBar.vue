6e866c48a19138d29dcb4e0d4d4b34299ec1f539
juns5779 <juns5779@naver.com>
2021-10-07 01:08:49 +0900

    feat:chat
    Former-commit-id: d8d73fad8370678e1d708d01c46f1d733b5ccc38

--- vue/src/components/SubNavBar.vue
+++ vue/src/components/SubNavBar.vue
@@ -87,7 +87,7 @@
 </template>
 
 <script>
-import { computed, ref, watch, reactive } from 'vue';
+import { computed, watch } from 'vue';
 import { useStore } from 'vuex';
 import { useRouter } from 'vue-router';
 
