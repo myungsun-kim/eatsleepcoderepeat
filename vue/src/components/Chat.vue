6c34bd0865d7ae393e7f02bf45eeb8ed4ac10fbc
neverte <neverte@naver.com>
2021-10-06 13:40:18 +0900

    chore: chat design
    Former-commit-id: e7ecc6cf317b2c69954930589387aed105e6d273

--- vue/src/components/Chat.vue
+++ vue/src/components/Chat.vue
@@ -2,30 +2,31 @@
   <el-row class="height5">
     <el-col :span="24" class="test-border"> Chat여백: </el-col>
   </el-row>
-  <input v-model="toId" @keyup.enter="changeMyId" />
   <el-row class="height90">
     <!-- 채팅방 목록 -->
-    <el-col :span="5" :offset="1" class="test-border">
+    <el-col :span="5" :offset="1" class="test-border chat-list">
+      <el-row class="height5"> </el-row>
+
       <el-row
-        v-for="session in chatList"
-        :key="session[0]"
-        class="height10"
+        v-for="(session, index) in chatList"
+        :key="index"
+        class="height10 chat-list-item"
         @click="changeSession(session[1])"
       >
-        {{ session[1].content }}
-        {{ session[1].senderId }}
-        {{ session[1].receiverId }}
-        {{ session[1].read_time }}
+        <!-- 보내는사람: {{ session[1].senderId }} <br /> -->
+        <el-col :span="4" class="flex-items">사진</el-col>
+        <el-col :span="16">
+          <el-row class="height50 flex-items chat-list-item-title">
+            받는 사람: {{ session[1].receiverId }}
+          </el-row>
+          <el-row class="height50 flex-items chat-list-item-msg">
+            내용: {{ session[1].content }}
+          </el-row>
+        </el-col>
+        <el-col :span="4" class="chat-list-item-msg"
+          >읽은 시간: {{ session[1].read_time }}</el-col
+        >
       </el-row>
-      <!-- <el-row class="height10">ROOM 2</el-row>
-      <el-row class="height10">ROOM 3</el-row>
-      <el-row class="height10">ROOM 4</el-row>
-      <el-row class="height10">ROOM 5</el-row>
-      <el-row class="height10">ROOM 6</el-row>
-      <el-row class="height10">ROOM 7</el-row>
-      <el-row class="height10">ROOM 8</el-row>
-      <el-row class="height10">ROOM 9</el-row>
-      <el-row class="height10">ROOM 10</el-row> -->
     </el-col>
     <el-tabs
       type="card"
@@ -59,45 +60,55 @@
       </div>
     </el-scrollbar>
     <!-- 채팅창 -->
-    <el-col :span="17" class="test-border">
-      <el-row v-for="msg in chatMessages" :key="msg.id" class="height10">
-        <el-col v-if="msg.receiverId == currentId && msg.type == 1">
-          {{ msg.content }}
-          {{ msg.sent_time }}
-          {{ msg.read_time }}
-          right
-        </el-col>
-        <el-col v-if="msg.receiverId != currentId && msg.type == 1">
-          left
-          {{ msg.content }}
-          {{ msg.sent_time }}
-          {{ msg.read_time }}
-          <!-- {{msg.read_time.getTime()}} -->
-          <p v-if="msg.read_time == 1000">읽지않음</p>
-        </el-col>
-      </el-row>
-
-      <el-row>
-        <el-col :span="20">
-          <div>
-            <el-input
-              type="text"
-              @keyup.enter="sendMessage"
-              v-model="message"
-              placeholder="메시지를 입력해주세요:)"
-              clearable
-            >
-            </el-input>
-          </div>
-        </el-col>
-        <el-col :span="2">
-          <el-button
-            @click="sendMessage"
-            icon="el-icon-s-promotion"
-            class="icon-m-p green-color-btn"
-          ></el-button>
-        </el-col>
-      </el-row>
+    <el-col :span="17" class="test-border chat-detail">
+      <div class="chat-log">
+        <!-- 채팅 내역 -->
+        <el-row v-for="msg in chatMessages" :key="msg.id" class="height10">
+          <el-col
+            v-if="msg.receiverId == currentId && msg.type == 1"
+            class="other-msg"
+          >
+            {{ msg.content }}
+            {{ msg.sent_time }}
+            {{ msg.read_time }}
+          </el-col>
+          <el-col
+            v-if="msg.receiverId != currentId && msg.type == 1"
+            class="my-msg"
+          >
+            {{ msg.content }} @ {{ new Date(msg.sent_time) }} 이건 substr(숫자,
+            숫자)으로 해결 가능@
+            {{ msg.read_time }}
+            <!-- {{msg.read_time.getTime()}} -->
+            <p v-if="msg.read_time == 1000">읽지않음</p>
+          </el-col>
+        </el-row>
+      </div>
+      <div>
+        <!-- 검색창 -->
+        <el-row>
+          <el-col :span="20">
+            <div>
+              <el-input
+                type="text"
+                @keyup.enter="sendMessage"
+                v-model="message"
+                placeholder="메시지를 입력해주세요:)"
+                clearable
+              >
+              </el-input>
+            </div>
+          </el-col>
+          <el-col :span="2">
+            <el-button
+              @click="sendMessage"
+              icon="el-icon-s-promotion"
+              class="icon-m-p green-color-btn"
+            ></el-button>
+          </el-col>
+        </el-row>
+      </div>
+      <!-- 검색창 끝 -->
     </el-col>
   </el-row>
 </template>
@@ -130,13 +141,6 @@ export default {
     const currentCounterPart = ref('');
     const chatList = computed(() => store.getters['chat/getChatList']);
     const chatMessages = computed(() => store.getters['chat/getMessages']);
-
-    const toId = ref('');
-
-    const changeMyId = () => {
-      console.log(toId.value);
-      store.commit('chat/setCurrentId', toId.value);
-    };
 
     // const chatList = [{content:"123"}, {content:"456"}];
     const store = useStore();
@@ -207,8 +211,6 @@ export default {
       chatList,
       chatMessages,
       currentCounterPart,
-      toId,
-      changeMyId,
 
       /*
         Functions
@@ -223,3 +225,65 @@ export default {
   },
 };
 </script>
+<style scoped>
+.chat-list-item {
+  background-color: #ffffff;
+}
+.chat-list-item-title {
+  font-family: Noto Sans KR;
+  font-style: normal;
+  font-weight: 500;
+  font-size: 14px;
+}
+.chat-list-item-msg {
+  font-family: Noto Sans KR;
+  font-style: normal;
+  font-weight: 500;
+  font-size: 12px;
+  color: rgba(0, 0, 0, 0.5);
+}
+
+.chat-list {
+  background-color: #ffffff;
+  border-radius: 15px 0px 0px 15px;
+}
+
+.chat-detail {
+  background: #f6f5f8;
+  border-radius: 0px 15px 15px 0px;
+  display: flex;
+  flex-flow: column;
+  justify-content: space-between;
+}
+
+.chat-log {
+  display: flex;
+  flex-flow: column;
+  justify-content: space-between;
+}
+
+.other-msg {
+  border-radius: 15px 15px 0px 15px;
+  text-align: left;
+  background: #ffffff;
+  color: #000000;
+
+  font-family: Noto Sans KR;
+  font-style: normal;
+  font-weight: 500;
+  font-size: 14px;
+  line-height: 20px;
+}
+.my-msg {
+  border-radius: 15px 0px 15px 15px;
+  text-align: right;
+  background: #7b9dff;
+  color: #ffffff;
+
+  font-family: Noto Sans KR;
+  font-style: normal;
+  font-weight: 500;
+  font-size: 14px;
+  line-height: 20px;
+}
+</style>
