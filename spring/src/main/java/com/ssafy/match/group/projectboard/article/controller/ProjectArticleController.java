67c64b347fd111e3a76035a6be1c02db42c0ce49
박범진 <qjawlsqjacks@naver.com>
2021-10-06 15:36:50 +0900

    fix: uri change
    Former-commit-id: 63768f5693e7c91a3f97a540c37db5ba34507713

--- spring/src/main/java/com/ssafy/match/group/projectboard/article/controller/ProjectArticleController.java
+++ spring/src/main/java/com/ssafy/match/group/projectboard/article/controller/ProjectArticleController.java
@@ -37,13 +37,13 @@ public class ProjectArticleController {
         return ResponseEntity.ok(projectArticleService.getProjectArticles(boardid, pageable));
     }
 
-    @GetMapping("/{boardid}/title/articles/{title}")
+    @GetMapping("/{boardid}/articles/title/{title}")
     @ApiOperation(value = "(프로젝트)게시물 리스트 제목 조회", notes = "<strong>받은 게시판 id와 제목</strong>을 사용해서 게시물들을 조회한다.")
     public ResponseEntity<Page<ProjectArticleListDto>> getArticlesByTitle(@PathVariable("boardid") Integer boardid, @PathVariable("title") String title, @PageableDefault(size = 10) @SortDefault(sort = "createDate", direction= Sort.Direction.DESC) Pageable pageable) throws Exception {
         return ResponseEntity.ok(projectArticleService.getProjectArticlesByTitle(boardid, title, pageable));
     }
 
-    @GetMapping("/{boardid}/nickname/articles/{nickname}")
+    @GetMapping("/{boardid}/articles/nickname/{nickname}")
     @ApiOperation(value = "(프로젝트)게시물 리스트 닉네임 조회", notes = "<strong>받은 게시판 id와 닉네임</strong>을 사용해서 게시물들을 조회한다.")
     public ResponseEntity<Page<ProjectArticleListDto>> getArticlesByNickname(@PathVariable("boardid") Integer boardid, @PathVariable("nickname") String nickname, @PageableDefault(size = 10) @SortDefault(sort = "createDate", direction= Sort.Direction.DESC) Pageable pageable) throws Exception {
         return ResponseEntity.ok(projectArticleService.getProjectArticlesByNickname(boardid, nickname, pageable));
